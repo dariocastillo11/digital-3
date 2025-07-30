bb96d928dae9ff86c19f98ba04d174498e62acb7
dariocastillo11 <dario.4979102@mail.com>
2025-07-30 09:38:27 -0300

    fixes v1.1

--- Display_autocounter/LPC1769_registers.c
+++ Display_autocounter/LPC1769_registers.c
@@ -1,9 +1,9 @@
 /**
 * @file LPC1769_reg.c
-* @brief Up-to-down BCD counter on the LPC1769 board using GPIO registers.
+* @brief BCD up-counter on the LPC1769 board using GPIO registers.
 * This program configures the 8 GPIO pins to drive a 7-segment display.
-* The main function is to count from 0 to 15 on the display and reset the count to zero.
-* A common cathode display with positive logic is used.
+* The main function is to count from 0 to 15 on the display and restart the count.
+* A common-cathode display with positive logic is used.
 */
 #include "LPC17xx.h"
 
@@ -14,7 +14,7 @@
 #define DELAY 2500
 
 /**
- * @brief Configures the GPIO pins for the RGB LEDs as outputs.
+ * @brief Configures the GPIO pins for the display as outputs.
  *
  * Sets the pin function to GPIO and configures the direction as output
  * for each color channel.
@@ -27,10 +27,10 @@ void configGPIO(void);
 void delay();
 
 /**
- * @brief Displays a number on the 7-segment display connected to GPIO pins.
+ * @def number
+ * @brief defines the BCD values for numbers 0-9 and letters A-F to be displayed on the 7-segment display.
  */
-void mostrarNumeros(int num);
-const uint32_t numeros[16] = {
+const uint32_t number[16] = {
     0x3F, // Number 0 in BCD
     0x06, // Number 1 in BCD
     0x5B, // Number 2 in BCD
@@ -48,33 +48,30 @@ const uint32_t numeros[16] = {
 	0X7b, // letter E in BCD
 	0X71  // letter F in BCD
 };
+
 /**
- * @brief Main function. Initializes GPIO and cycles through RGB LED color sequence.
- *
- * The sequence includes primary colors (red, green, blue), secondary colors (cyan, magenta, yellow),
- * and white (all LEDs on), repeating indefinitely.
- * @return int Always returns 0 (never reached).
- */
+* @brief Main function. Initializes GPIO and creates a loop to iterate through the constant  
+* of 16 hexadecimal values that are displayed on the pins connected to the 7-segment display.
+* @return int Always returns 0 (never reached).
+*/
 int main(void) {
     configGPIO();
-
     while (1) {
     for (int i = 0; i < 16; i++) {
         LPC_GPIO0->FIOCLR = 0xFF;
-        LPC_GPIO0->FIOSET = numeros[i];
+        LPC_GPIO0->FIOSET = number[i];
         delay();
     }
     }
 }
 void configGPIO(void) {
+    LPC_PINCON->PINSEL0 &= ~(0xFFFF);  // P0.0 to P0.7 as GPIO        
+    LPC_PINCON->PINMODE0 &= ~(0x0000FFFF);  // clear 16 pin of pin mode
+    LPC_PINCON->PINMODE0 |=  (0x0000AAAA);  // desactivate pull-up/pull-down resistors with option -01-
+                                            // 0X AAAA = 1010 1010 1010 
     LPC_GPIO0 ->FIODIR |= 0xFF;       // Set P0.0 a P0.7 as output
 }
-
 void delay() {
     for(uint32_t i=0; i<DELAY; i++)
         for(uint32_t j=0; j<DELAY; j++);
 }
-
-
-
-
